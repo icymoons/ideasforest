2ecc9aa4030cba4e646d2a4c1d1fbe87ba280e89
Aurélien de Peyrelongue <aurelien.depeyrelongue@gmail.com>
2019-04-02 18:43:54 +0200

    Add vuetify

---
+++ client/src/components/IdeaBloc.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-card class="mx-auto" color="#26c6da" dark>
+    <v-card-title>
+      <span>data</span>
+    </v-card-title>
+
+    <v-card-text class="headline font-weight-bold">{{label}}</v-card-text>
+
+    <v-card-actions>
+      <v-list-tile class="grow">
+        <v-list-tile-avatar color="grey darken-3">
+          <v-img
+            class="elevation-6"
+            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+          ></v-img>
+        </v-list-tile-avatar>
+
+        <v-list-tile-content>
+          <v-list-tile-title>{{author}}</v-list-tile-title>
+        </v-list-tile-content>
+
+        <v-layout align-center justify-end>
+          <v-icon class="mr-1">mdi-heart</v-icon>
+          <span class="subheading mr-2">256</span>
+          <span class="mr-1">·</span>
+          <v-icon class="mr-1">mdi-share-variant</v-icon>
+          <span class="subheading">45</span>
+        </v-layout>
+      </v-list-tile>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    author: String,
+    likes: Number
+  },
+  data: () => ({})
+};
+</script>
+
+<style>
+.v-card {
+  margin-bottom: 12px;
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container align-center>
+    <v-layout wrap justify-center row>
+      <v-flex xs9>
+        <v-layout wrap row>Filter</v-layout>
+        <v-layout wrap row>
+          <v-flex xs4 v-for="(idea,index) in ideasComputed" :key="index" :class="'column-ideas'">
+            <idea-bloc v-bind="idea"></idea-bloc>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import IdeaBloc from "../components/IdeaBloc";
 
 export default {
-  name: 'home',
   components: {
-    HelloWorld
+    IdeaBloc
+  },
+
+  data: function() {
+    return {
+      ideas: [
+        {
+          label: "idee",
+          author: "author",
+          likes: 0
+        }
+      ]
+    };
+  },
+  computed: {
+    ideasComputed() {
+      var arr = [];
+      for (let i = 0; i < 50; i++) {
+        arr.push({
+          label: "idee" + i,
+          author: "author" + i,
+          likes: 0 + i
+        });
+      }
+      return arr;
+    }
   }
-}
+};
 </script>
+<style>
+.column-ideas {
+  padding-left: 6px;
+  padding-right: 6px;
+}
+</style>
+
