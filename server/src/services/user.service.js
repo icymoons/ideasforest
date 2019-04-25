de46beda042d248ce03c4d896f537c8ef56d76dc
Aurélien de Peyrelongue <aurelien.depeyrelongue@gmail.com>
2019-04-25 18:14:18 +0200

    Add replies create and comment create

--- server/src/services/user.service.js
+++ server/src/services/user.service.js
@@ -3,32 +3,29 @@ const { users } = require('../models')
 module.exports = {
     async getUser(userId) {
         try {
-            const user = await users.findOne({
+            return await users.findOne({
                 where: {
                     id: userId
                 }
             })
-            return user
         } catch (error) {
             throw new Error(error)
         }
     },
     async createUser(firstName, lastName, pseudo, email, password) {
         try {
-            const user = await users.create({
+            return await users.create({
                 firstName, lastName, pseudo, email, password
             })
-            return user
         } catch (error) {
             throw new Error(error)
         }
     },
     async updateUser(userId, firstName, lastName, pseudo, email, password) {
         try {
-            const user = await users.updateOne({
+            return await users.updateOne({
                 userId, firstName, lastName, pseudo, email, password
             })
-            return user
         } catch (error) {
             throw new Error(error)
         }
